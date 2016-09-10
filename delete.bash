f0647ecb45200552b69d76b38d9e3c9e4e77ad28
Jeff Puckett <jeff@jeffpuckett.com>
2016-09-09 22:07:44 -0500

    add alias git clean merged remote branches

--- delete.bash
+++ delete.bash
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-clean_remote(){
+git_clean_merged_remote(){
 	if [ -z $1 ]; then echo "error: remote required"; return; fi
 	if [ -z $2 ]; then echo "error: branch required"; return; fi
 	git branch -a --merged remotes/$1/$2 | grep -v $2 | grep "remotes/$1/" | cut -d "/" -f 3 | xargs -n 1 git push --delete $1
 }
+
+alias gcr='git_clean_merged_remote'
+__git_complete gcr _git_pull
